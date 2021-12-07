be7053d65992834839eadf89c8d7d43e201796e3
ckaynatividad <natividad.ckay@gmail.com>
2021-12-07 14:16:13 -0800

    styling, added more options to drop down

--- src/App.js
+++ src/App.js
@@ -18,21 +18,29 @@ function App() {
     fetchData();
   }, []);
   const filterFlags = () => {
-    return flags.filter((flag) => flag.name.includes(query) && (flag.sort === sort || sort === 'all'));
+    return flags.filter((flag) => flag.name.includes(query) && (flag.continent === sort || sort === 'all'));
   };
 
   return (
     <section className="App">
-      <h1>Countries</h1>
+      <h1>Flags of the World</h1>
       <input type="text" value={query} onChange={(e) => { setQuery(e.target.value);
       }} />
       <select value ={sort} onChange={e=>setSort(e.target.value)}>
         <option value="all">All</option>
-        <option value="name">Name</option>
+        <option value="Asia">Asia</option>
+        <option value="Oceania">Oceania</option>
+        <option value="Africa">Africa</option>
+        <option value="Europe">Europe</option>
+        <option value="North America">North America</option>
+        <option value="Antarctica">Antarctica</option>
+        <option value="South America">South America</option>
       </select>
-      {filterFlags().map((flag) => {
-        return <FlagsCard key={flag.id} {...flag} />;
-      })}
+      <div className="flags">
+        {filterFlags().map((flag) => {
+          return <FlagsCard key={flag.id} {...flag} />;
+        })}
+      </div>
     </section>
   );
 }
--- src/components/FlagCard.js
+++ src/components/FlagCard.js
@@ -5,8 +5,8 @@ export default function FlagCard({ name, iso2 }) {
   return (
     <>
       <div>
+        <h3>{name}</h3>
         <img src={`https://flagcdn.com/80x60/${iso2.toLowerCase()}.png`} />
-        <h2>{name}</h2>
       </div>
     </>
   );
